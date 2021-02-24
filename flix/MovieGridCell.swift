4ea5746d44cddb6d14f3e54cbc7a024dd82566b5
Kaileyv <kailey.van@stonybrook.edu>
2021-02-24 00:16:06 -0500

    commit collectionView changes

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Kailey Van on 2/23/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
